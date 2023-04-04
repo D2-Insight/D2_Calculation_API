e5e3c9a1baa51c2c1a86d977d5fb7f59a005674c
Harmon Brammer <harm@brammer.dev>
2023-04-03 21:24:51 -0400

    actually fix bows :)

--- src/perks/mod.rs
+++ src/perks/mod.rs
@@ -1045,8 +1045,10 @@ pub fn get_firing_modifier(
         firing_modifier.inner_burst_scale *= tmp.inner_burst_scale;
         firing_modifier.burst_size_add += tmp.burst_size_add;
     }
-    if _input_data.weapon_type == &WeaponType::BOW && firing_modifier.burst_delay_add < 0.5 {
-        firing_modifier.burst_delay_add = 0.5;
+    if _input_data.weapon_type == &WeaponType::BOW
+        && _input_data.curr_firing_data.burst_delay + firing_modifier.burst_delay_add < 0.5
+    {
+        firing_modifier.burst_delay_add = 500.0 - _input_data.curr_firing_data.burst_delay;
     }
     firing_modifier
 }
